56bea1152e094712ecb52f2062e46836ee0811e5
manzanofp <felipemanzanofp@gmail.com>
2023-04-25 17:28:17 -0300

    Add Database, Create DbContext, Create table in DB using Migration

---
+++ GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{         //essa é a classe responsável por criar o banco de dados
+    public class MySqlContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+
+        public MySqlContext(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("ProductApiDataBase");
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+        }
+            //aqui estou pedindo para ele criar a tabela da classe products dentro do banco
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
